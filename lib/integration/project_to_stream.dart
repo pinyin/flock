9e3455e27c18ac9c3021588a3e6585dd4a5dc677
pinyin <bob432091@gmail.com>
2019-04-13 20:41:04 +0800

    update projectToStream

--- lib/integration/project_to_stream.dart
+++ lib/integration/project_to_stream.dart
@@ -2,28 +2,31 @@ import 'dart:async';
 
 import 'package:flock/flock.dart';
 
-Stream<P> projectToStream<P, E>(Store<E> store, Projector<P, E> projector) {
-  var refCount = 0;
-  var latestProjection = store.project(projector);
+Stream<P> Function(Store<E>) projectToStream<P, E>(Projector<P, E> projector) {
+  return (Store<E> store) {
+    var refCount = 0;
+    var latestProjection = store.project(projector);
 
-  final controller = StreamController<P>();
+    final controller = StreamController<P>();
 
-  void emit() {
-    final currentProjection = store.project(projector);
-    if (currentProjection != latestProjection)
-      controller.add(currentProjection);
-    latestProjection = currentProjection;
-  }
+    // todo batch update
+    void emit() {
+      final currentProjection = store.project(projector);
+      if (currentProjection != latestProjection)
+        controller.add(currentProjection);
+      latestProjection = currentProjection;
+    }
 
-  controller.onListen = () {
-    refCount++;
-    if (refCount == 1) store.addListener(emit);
+    controller.onListen = () {
+      refCount++;
+      if (refCount == 1) store.addListener(emit);
+    };
+
+    controller.onCancel = () {
+      refCount--;
+      if (refCount == 0) store.removeListener(emit);
+    };
+
+    return controller.stream;
   };
-
-  controller.onCancel = () {
-    refCount--;
-    if (refCount == 0) store.removeListener(emit);
-  };
-
-  return controller.stream;
 }
