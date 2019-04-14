f9998e6ee40c1cbf40e574fad58bf482dbcf9db4
pinyin <bob432091@gmail.com>
2019-04-14 18:59:53 +0800

    skip projection equality check

--- lib/integration/project_to_stream.dart
+++ lib/integration/project_to_stream.dart
@@ -5,16 +5,12 @@ import 'package:flock/flock.dart';
 Stream<P> Function(Store<E>) projectToStream<P, E>(Projector<P, E> projector) {
   return (Store<E> store) {
     var refCount = 0;
-    P latestProjection = null;
 
     final controller = StreamController<P>();
 
     // todo batch update
     void emit() {
-      final currentProjection = store.project(projector);
-      if (currentProjection != latestProjection)
-        controller.add(currentProjection);
-      latestProjection = currentProjection;
+      controller.add(store.project(projector));
     }
 
     controller.onListen = () {
