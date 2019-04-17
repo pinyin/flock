bf39c192d786165aac8b2694d0fff4c00eda7498
pinyin <bob432091@gmail.com>
2019-04-17 21:33:50 +0800

    add StoreProxyBase

---
+++ lib/enhancers/store_proxy_base.dart
@@ -0,0 +1,44 @@
+import 'package:flock/base/types.dart';
+import 'package:flock/flock.dart';
+
+abstract class StoreProxyBase<E> extends StoreForEnhancer<E> {
+  final StoreForEnhancer<E> inner;
+
+  StoreProxyBase(this.inner);
+
+  @override
+  void addListener(listener) {
+    inner.addListener(listener);
+  }
+
+  @override
+  int get cursor => inner.cursor;
+
+  @override
+  List<E> get events => inner.events;
+
+  @override
+  P project<P>(projector) {
+    return inner.project(projector);
+  }
+
+  @override
+  void publish(E event) {
+    inner.publish(event);
+  }
+
+  @override
+  void removeListener(listener) {
+    inner.removeListener(listener);
+  }
+
+  @override
+  void replaceEvents(List<E> events, [int cursor]) {
+    inner.replaceEvents(events, cursor);
+  }
+
+  @override
+  subscribe(subscriber) {
+    return inner.subscribe(subscriber);
+  }
+}
