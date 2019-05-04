c668f67cc189bc939c8f4c3d1a472c9d89da4fb0
pinyin <bob432091@gmail.com>
2019-05-04 09:20:37 +0800

    automatically terminate stream passed to effect

--- lib/enhancers/with_side_effect.dart
+++ lib/enhancers/with_side_effect.dart
@@ -15,31 +15,38 @@ Stream<E> emptySideEffect<E>(Stream<E> events, Store<E> store) async* {}
 
 class _Proxy<E> extends StoreProxyBase<E> {
   _Proxy(StoreForEnhancer<E> inner, this.sideEffect) : super(inner) {
-    _resubscribe();
+    resubscribe();
   }
 
   @override
   E publish(E event) {
-    _incoming.add(event);
+    incoming.add(event);
     return inner.publish(event);
   }
 
   @override
   void replaceEvents(List<E> events, [int cursor]) {
     inner.replaceEvents(events, cursor);
-    _resubscribe();
+    resubscribe();
   }
 
-  StreamSubscription<E> _subscription;
+  StreamSubscription<E> subscription;
+  StreamController<E> incoming;
 
-  void _resubscribe() {
-    if (_subscription is StreamSubscription<E>) _subscription.cancel();
-    _subscription = _incoming.stream
+  void resubscribe() {
+    if (subscription is StreamSubscription<E>) {
+      incoming.close();
+      subscription.cancel();
+      subscription = null;
+    }
+
+    incoming = StreamController();
+
+    subscription = incoming.stream
         .transform(
             StreamTransformer.fromBind((stream) => sideEffect(stream, this)))
         .listen(publish);
   }
 
-  final _incoming = StreamController<E>();
   final SideEffect<E> sideEffect;
 }
